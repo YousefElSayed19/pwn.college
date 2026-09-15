6f97692d3f32088b1f65dbf00834243a0504775d
YousefElSayed19 <yousafe2042005@gmail.com>
2026-09-15 15:40:37 +0300

    read flag

--- Assembly/reverse/outputPasswordAfterRevUsegdb.s
+++ Assembly/reverse/outputPasswordAfterRevUsegdb.s
@@ -1,3 +1,25 @@
 @ after open program by gdb reverse_me
 @ and start and disassemble _start
 @ i found 
+0x0000000000401000 <+0>: mov 0x10(%rsp),%rcx 
+0x0000000000401005 <+5>: xor %eax,%eax 
+x0000000000401007 <+7>: mov (%rcx),%al 
+0x0000000000401009 <+9>: mov 0x401088(,%rax,8),%rax 
+0x0000000000401011 <+17>: jmp *%rax
+
+@ and just write x/256a 0x401088 to show the table of address
+
+@ 0x401238 → fail
+@ 0x401240 → success
+
+@ entry = 8 bytes.
+
+
+@ 0x401240
+@ 0x401240 - 0x401088
+@ = 0x1b8
+@ 0x1b8 / 8 = 55
+
+@ 55 ASCII :
+
+@ 55 = '7'
---
+++ Assembly/reverse/readFlagDirAfterGetPassword.s
@@ -0,0 +1,73 @@
+0000000000401000 <_start>:
+
+401000: 48 8b 7c 24 10          mov rdi,QWORD PTR [rsp+0x10]
+
+401005: c6 04 24 70             mov BYTE PTR [rsp],0x70
+401009: c6 44 24 01 6e          mov BYTE PTR [rsp+0x1],0x6e
+40100e: c6 44 24 02 45          mov BYTE PTR [rsp+0x2],0x45
+401013: c6 44 24 03 55          mov BYTE PTR [rsp+0x3],0x55
+401018: c6 44 24 04 47          mov BYTE PTR [rsp+0x4],0x47
+40101d: c6 44 24 05 53          mov BYTE PTR [rsp+0x5],0x53
+401022: c6 44 24 06 00          mov BYTE PTR [rsp+0x6],0x0
+
+401027: 48 8d 34 24             lea rsi,[rsp]
+
+
+000000000040102b <loop>:
+
+40102b: 8a 06                   mov al,BYTE PTR [rsi]
+40102d: 3a 07                   cmp al,BYTE PTR [rdi]
+40102f: 75 6e                   jne 40109f <fail>
+
+401031: 3c 00                   cmp al,0x0
+401033: 74 08                   je 40103d <success>
+
+401035: 48 ff c7                inc rdi
+401038: 48 ff c6                inc rsi
+40103b: eb ee                   jmp 40102b <loop>
+
+
+000000000040103d <success>:
+
+40103d: c6 04 24 2f             mov BYTE PTR [rsp],0x2f
+401041: c6 44 24 01 66          mov BYTE PTR [rsp+0x1],0x66
+401046: c6 44 24 02 6c          mov BYTE PTR [rsp+0x2],0x6c
+40104b: c6 44 24 03 61          mov BYTE PTR [rsp+0x3],0x61
+401050: c6 44 24 04 67          mov BYTE PTR [rsp+0x4],0x67
+401055: c6 44 24 05 00          mov BYTE PTR [rsp+0x5],0x0
+
+40105a: 48 89 e7                mov rdi,rsp
+40105d: 48 c7 c6 00 00 00 00    mov rsi,0x0
+401064: 48 c7 c0 02 00 00 00    mov rax,0x2
+40106b: 0f 05                   syscall
+
+40106d: 48 89 c7                mov rdi,rax
+401070: 48 89 e6                mov rsi,rsp
+401073: 48 c7 c2 40 00 00 00    mov rdx,0x40
+40107a: 48 c7 c0 00 00 00 00    mov rax,0x0
+401081: 0f 05                   syscall
+
+401083: 48 89 c2                mov rdx,rax
+401086: 48 c7 c7 01 00 00 00    mov rdi,0x1
+40108d: 48 c7 c0 01 00 00 00    mov rax,0x1
+401094: 0f 05                   syscall
+
+401096: 48 c7 c0 3c 00 00 00    mov rax,0x3c
+40109d: 0f 05                   syscall
+
+
+000000000040109f <fail>:
+
+40109f: 48 c7 c0 3c 00 00 00    mov rax,0x3c
+4010a6: 0f 05                   syscall
+
+
+@ 0x70 = p
+@ 0x6e = n
+@ 0x45 = E
+@ 0x55 = U
+@ 0x47 = G
+@ 0x53 = S
+@ 0x00 = null terminator
+
+@ then password is pnEUGS
