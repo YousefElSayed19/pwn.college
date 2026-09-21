cd4adf5e76a126120bd1f7b1aa809cdded67983a
YousefElSayed19 <yousafe2042005@gmail.com>
2026-09-21 14:03:42 +0300

    edit iterative

--- Assembly/build web server/iterative-get server.s
+++ Assembly/build web server/iterative-get server.s
@@ -7,7 +7,7 @@ _start:
     # socket(AF_INET, SOCK_STREAM, 0)
     mov rdi, 2
     mov rsi, 1
-    mov rdx, 0
+    xor rdx, rdx
     mov rax, 41
     syscall
 
@@ -16,26 +16,27 @@ _start:
 
     # sockaddr_in
     sub rsp, 16
-
     mov word ptr [rsp], 2
-    mov word ptr [rsp+2], 0x5000
-    mov qword ptr [rsp+4], 0
+    mov word ptr [rsp + 2], 0x5000
+    mov qword ptr [rsp + 4], 0
 
-    # bind(sockfd, sockaddr, 16)
+    # bind
     mov rdi, r12
     mov rsi, rsp
     mov rdx, 16
     mov rax, 49
     syscall
 
-    # listen(sockfd, 0)
+    # listen
     mov rdi, r12
     xor rsi, rsi
     mov rax, 50
     syscall
 
+
 request_loop:
-    # accept(sockfd, NULL, NULL)
+
+    # accept(listening_socket, NULL, NULL)
     mov rdi, r12
     xor rsi, rsi
     xor rdx, rdx
@@ -45,21 +46,20 @@ request_loop:
     # Save client socket
     mov r13, rax
 
-    # Request buffer
+    # Allocate request/file buffer
     sub rsp, 1024
     mov r14, rsp
 
-    # read(client_fd, request, 1024)
+    # read(client_socket, buffer, 1024)
     mov rdi, r13
     mov rsi, r14
     mov rdx, 1024
     xor rax, rax
     syscall
 
-    # Path starts after "GET "
+    # GET /path HTTP/1.1
+    # Path starts at buffer + 4
     lea r15, [r14 + 4]
-
-    # Find space after path
     mov rbx, r15
 
 find_end:
@@ -69,6 +69,7 @@ find_end:
     jmp find_end
 
 found_end:
+    # Null terminate path
     mov byte ptr [rbx], 0
 
     # open(path, O_RDONLY, 0)
@@ -78,48 +79,49 @@ found_end:
     mov rax, 2
     syscall
 
-    # Save file fd
-    mov r12, rax
+    # Save file descriptor
+    mov r8, rax
 
-    # Read file
-    mov rdi, r12
+    # read(file_fd, buffer, 1024)
+    mov rdi, r8
     mov rsi, r14
     mov rdx, 1024
     xor rax, rax
     syscall
 
     # Save file size
-    mov rbx, rax
+    mov r9, rax
 
-    # close(file)
-    mov rdi, r12
+    # close(file_fd)
+    mov rdi, r8
     mov rax, 3
     syscall
 
-    # write(client, HTTP response, 19)
+    # write(client_socket, HTTP header, 19)
     mov rdi, r13
     lea rsi, [rip + response]
     mov rdx, 19
     mov rax, 1
     syscall
 
-    # write(client, file content, file size)
+    # write(client_socket, file_content, file_size)
     mov rdi, r13
     mov rsi, r14
-    mov rdx, rbx
+    mov rdx, r9
     mov rax, 1
     syscall
 
-    # close(client)
+    # close(client_socket)
     mov rdi, r13
     mov rax, 3
     syscall
 
-    # Restore stack space used for request buffer
+    # Free request buffer
     add rsp, 1024
 
-    # Handle next request
+    # Accept next client
     jmp request_loop
+
 
 .section .data
 
